01da378e9dbc7874b112da751d6fa051e5c646e5
Andrew Wu <andrew_0564@hotmail.com>
2019-12-04 21:42:14 -0500

    comments

--- src/containers/BeanMixContainer.js
+++ src/containers/BeanMixContainer.js
@@ -30,9 +30,12 @@ class BeanMixContainer extends React.Component {
         this.randomChange=this.randomChange.bind(this)
         this.handleSubmit=this.handleSubmit.bind(this)
     }
+    //handles the check value of the checkboxes
     handleCheck(id) {
         this.setState(prevState => {
+            //every bean in the coffeeBeans list in the state has an id,
             const updatedCoffeeBeans = prevState.coffeeBeans.map(bean => {
+                //map goes through the list of Beans to find the id that matches
                 if (id === bean.id) {
                     bean.added = !bean.added
                 }
@@ -43,46 +46,51 @@ class BeanMixContainer extends React.Component {
             }
         })
     }
-
+    //general handleChange
     handleChange(event) {
-        const {name, type, value, key} = event.target
+        const {name, value} = event.target
         this.setState({
             [name]: value
         })  
     }
-
+    //randomly change the state
     randomChange() {
+        //random roast using Math.random() to get a random index
         const roasts = ['light', 'medium', 'dark']
         const randomRoast = roasts[Math.round(Math.random()*2)]
+        //random Beans
         const torf = [true,false]
         this.setState(prevState => {
             const randomBeans = prevState.coffeeBeans.map(bean => {
+                //random true or false 
                 if (torf[Math.round(Math.random())]) {
-                bean.added = !bean.added 
+                //if true, the value of added changes
+                    bean.added = !bean.added 
                 }
                 return bean
             })
+            //return new state with new value of coffeeBeans
             return {coffeeBeans: randomBeans}
         })         
         this.setState({
+            //return new state with new value of Roast
             roast: randomRoast
-
         })
     }
-
+    //handleSubmit changes the value isSubmit to true
+    //When isSubmit is true, the order is rendered the user interface
     handleSubmit(event) {
         event.preventDefault()
         this.setState({isSubmit: true})
-
-
     }
-
+ 
     render() {
         const BeanMixData = this.state.coffeeBeans.map(bean =>
             <BeanMixComponent
                     key={bean.id}
                     bean={bean}
                     handleCheck={this.handleCheck}/>)
+        //map the beans that have added = true to an html component to be rendered
         const BeanDisplay = this.state.coffeeBeans.map(bean => bean.added ? <p>{bean.name} </p> : null)
         return(
             <div className='beanList'>
